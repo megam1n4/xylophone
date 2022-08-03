b154fdba1ca2e09fd3c06e1b7bb1cf7452c39b4b
m3gam1n4 <rdebnath086@gmail.com>
2022-08-03 16:02:19 +0600

    final view

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,7 +12,6 @@ import 'package:xylophone/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const Xylophone());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
